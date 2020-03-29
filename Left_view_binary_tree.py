f8e7485bc31a4abb2b0621bf84188e3a73211d64
panwarv127@gmail.com <panwarv127@gmail.com>
2020-03-29 15:16:46 +0530

    leftViewBinaryTree

---
+++ Left_view_binary_tree.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+def leftView(root,level,max_level):
+    if root is None:
+        return
+    
+    if(max_level[0]<level):
+        print(max_level,level)
+        print("%d\t"%(root.data))
+        max_level[0]=level
+        
+
+    leftView(root.left,level+1,max_level)
+    leftView(root.right,level+1,max_level)
+    
+  
+    
+
+
+
+
+root = Node(12) 
+root.left = Node(10) 
+root.right = Node(20) 
+root.right.left = Node(25) 
+root.right.right = Node(40) 
+max_level=[0]
+leftView(root, 1, max_level) 
