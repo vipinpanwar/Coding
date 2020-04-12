5b0d94c852921a7cb6d7f75f2459d54aca4c7c3e
panwarv127@gmail.com <panwarv127@gmail.com>
2020-04-12 22:30:12 +0530

    Running Median

--- RunningSumMedian.java
+++ RunningSumMedian.java
@@ -47,7 +47,7 @@ public class RunningSumMedian {
     
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args){
+    public static void main(String[] args){  
         int n =scanner.nextInt();
         int arr[]=new int[n];
         for(int i=0;i<n;i++)arr[i]=scanner.nextInt();
